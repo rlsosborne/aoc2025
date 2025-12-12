cb54f81f5de8746167ed35e22bad541c65c71eaa
Richard Osborne <rlsosborne@gmail.com>
2025-12-12 17:02:35 +0000

    Day 10 part 2

--- src/bin/10.rs
+++ src/bin/10.rs
@@ -1,6 +1,7 @@
 use aoc2025::util::{get_input_path, read_lines};
 use std::collections::HashSet;
 
+#[derive(Debug)]
 struct Problem {
     light_goal: Vec::<bool>,
     joltage_goal: Vec::<u32>,
@@ -57,10 +58,184 @@ fn solve_lights(problem: &Problem) -> usize {
     panic!()
 }
 
+#[derive(PartialEq)]
+enum PropagateResult {
+    NoChange,
+    Change,
+    Unsat
+}
+
+impl std::ops::BitOr for PropagateResult {
+    type Output = Self;
+
+    fn bitor(self, rhs: Self) -> Self::Output {
+        match (self, rhs) {
+            (PropagateResult::Unsat, _) => PropagateResult::Unsat,
+            (_, PropagateResult::Unsat) => PropagateResult::Unsat,
+            (PropagateResult::NoChange, PropagateResult::NoChange) => PropagateResult::NoChange,
+            _ => PropagateResult::Change
+        }
+    }
+}
+
+#[derive(Debug, Clone)]
+struct Domain {
+    low: i32,
+    high: i32
+}
+
+impl Domain {
+    fn update_low(&mut self, value: i32) -> PropagateResult {
+        if value > self.high {
+            PropagateResult::Unsat
+        } else if value > self.low {
+            self.low = value;
+            PropagateResult::Change
+        } else {
+            PropagateResult::NoChange
+        }
+    }
+    fn update_high(&mut self, value: i32) -> PropagateResult {
+        if value < self.low {
+            PropagateResult::Unsat
+        } else if value < self.high {
+            self.high = value;
+            PropagateResult::Change
+        } else {
+            PropagateResult::NoChange
+        }
+    }
+}
+
+trait Constrait {
+    fn propagate(&self, domains: &mut Vec::<Domain>) -> PropagateResult;
+}
+
+struct SumConstraint {
+    vars: Vec::<usize>,
+    dst: usize
+}
+
+impl Constrait for SumConstraint {
+    fn propagate(&self, domains: &mut Vec<Domain>) -> PropagateResult {
+        let sum_low = self.vars.iter().map(|&v| domains[v].low).sum();
+        let sum_high = self.vars.iter().map(|&v| domains[v].high).sum();
+        let result = domains[self.dst].update_low(sum_low);
+        let result = result | domains[self.dst].update_high(sum_high);
+        let dst_low = domains[self.dst].low;
+        let dst_high = domains[self.dst].high;
+        let result = self.vars.iter().fold(result, |result, &v| {
+            let new_low = dst_low - sum_high + domains[v].high;
+            let new_high = dst_high - sum_low + domains[v].low;
+            result | domains[v].update_low(new_low) | domains[v].update_high(new_high)
+        });
+        result
+    }
+}
+
+fn solve(mut domains: Vec<Domain>, constraints: &Vec<Box<dyn Constrait>>) -> Option<Vec<Domain>> {
+    loop {
+        let mut result = PropagateResult::NoChange;
+        for constraint in constraints {
+            result = result | constraint.propagate(&mut domains);
+            if result == PropagateResult::Unsat {
+                return None;
+            }
+        }
+        if result == PropagateResult::NoChange {
+            break;
+        }
+    }
+    if let Some((i, _)) = domains.iter().enumerate().filter(|(_, domain)| {
+        domain.low != domain.high
+    }).min_by_key(|(_, domain)| {
+        domain.high - domain.low
+    }) {
+        for value in (domains[i].low..domains[i].high+1).rev() {
+            let mut new_domains = domains.clone();
+            new_domains[i].low = value;
+            new_domains[i].high = value;
+            if let Some(solution) = solve(new_domains, constraints) {
+                return Some(solution);
+            }
+        }
+        return None
+    } else {
+        return Some(domains)
+    }
+}
+
+#[derive(Clone, Copy)]
+struct Var {
+    index: usize
+}
+
+#[derive(Default)]
+struct Solver {
+    domains: Vec<Domain>,
+    constraints: Vec<Box<dyn Constrait>>
+}
+
+impl Solver {
+    fn add_var(&mut self, low: i32, high: i32) -> Var {
+        let index = self.domains.len();
+        self.domains.push(Domain{low:low, high:high});
+        Var{index}
+    }
+    fn sum_eq(&mut self, vars: &[Var], dst: Var) {
+        let vars: Vec<usize> = vars.iter().map(|Var{ index: v }| *v).collect();
+        let Var { index: dst} = dst;
+        self.constraints.push(Box::new(SumConstraint{vars:vars, dst:dst}));
+    }
+    fn minimise(&self, var: Var) -> Option<i32> {
+        let mut best = None;
+        loop {
+            let mut new_domains = self.domains.clone();
+            if let Some(best) = best {
+                if new_domains[var.index].update_high(best - 1) == PropagateResult::Unsat {
+                    break;
+                }
+            }
+            let Some(solution) = solve(new_domains, &self.constraints) else { break; };
+            best = Some(solution[var.index].low);
+            println!("best: {}", solution[var.index].low);
+        }
+        best
+    }
+}
+
+fn solve_joltage(problem: &Problem) -> usize {
+    println!("problem: {:?}", problem);
+    let mut solver = Solver::default();
+    let num_buttons = problem.buttons.len();
+    let num_counters = problem.joltage_goal.len();
+    let mut counters_to_buttons = vec!(Vec::<usize>::default();num_counters);
+    for (i, counters) in problem.buttons.iter().enumerate() {
+        for &counter in counters {
+            counters_to_buttons[counter].push(i)
+        }
+    }
+    let vars: Vec<Var> = (0..num_buttons).map(|_| {
+        solver.add_var(0, 100000)
+    }).collect();
+    for (&goal, buttons) in problem.joltage_goal.iter().zip(counters_to_buttons) {
+        let sum = solver.add_var(goal as i32, goal as i32);
+        let vars: Vec<Var> = buttons.iter().map(|&i| vars[i]).collect();
+        solver.sum_eq(&vars, sum);
+    }
+    let sum = solver.add_var(0, 100000);
+    solver.sum_eq(&vars, sum);
+    solver.minimise(sum).unwrap() as usize
+}
+
 fn main() {
     let lines = read_lines(get_input_path().join("input.txt"));
     let problems = lines.iter().map(parse_problem);
     let light_solutions = problems.clone().map(|problem| solve_lights(&problem));
     let light_total: usize = light_solutions.sum();
     println!("Light total: {}", light_total);
+    // This took 25m31.208s on my 2020 Macbook Pro!
+    let joltage_solutions = problems.map(|problem| solve_joltage(&problem));
+    let joltage_total: usize = joltage_solutions.sum();
+    println!("Joltage total: {}", joltage_total);
 }
